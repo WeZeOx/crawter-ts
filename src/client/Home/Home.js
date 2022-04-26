c5fe47a5b524909b67b1d51c679aebee8db7c04a
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-26 09:34:21 +0200

    feat(dev): beginning of front end

--- src/client/Home/Home.js
+++ src/client/Home/Home.js
@@ -1,22 +1,32 @@
-
 const fetchData = () => {
   return fetch('http://localhost:3000/api')
     .then((r) => r.json())
     .catch((err) => console.log(err))
 }
 const data = Object.values(await fetchData())
+const containerCard = document.getElementById('containerCard')
+document.getElementById('inputSearch').addEventListener("input", (e) => {
+  const value = e.target.value.toUpperCase()
+  cardData.forEach((trend) => {
+    const isVisible = trend.category.includes(value)
+    trend.element.classList.toggle("hide", !isVisible)
+  })
+});
 
-data.forEach((item) => {
-  console.log(item)
-
+const cardData = data.map((item, index) => {
   const img = item.TrendTop
+  const category = item.Category
+
+  const content = `<div class="card">
+    <span class="top">TOP #${index + 1}</span>
+    <div class="containerImg"><img src="${img}" alt="Image Trend"/></div>
+    <span class="category">${category}</span>
+</div>`
+
+  const htmlObject = document.createElement('div');
+  htmlObject.innerHTML = content;
+  containerCard.appendChild(htmlObject)
+  return {category: category.toUpperCase(), element: htmlObject, id: index}
 
 
-
-  // img.forEach((image) => {
-    let newDiv = document.createElement('div')
-    newDiv.style.background = `url(${img})`
-    newDiv.className = "test"
-    document.body.appendChild(newDiv)
-  // })
 })
--- src/serveur/CrawlerBot/bot.writejson.ts
+++ src/serveur/CrawlerBot/bot.writejson.ts
@@ -23,11 +23,12 @@ export const botWriteJson = (arg: (string | (string | null)[] | null)[][]) => {
       })
     idx++
   }
+  
   const date = new Date()
   let hour: string = date.getHours().toString()
   if (hour.length === 1) hour = '0' + hour
   twitchTrendArray.push({"DateLaunch": `${date.getDate()}\/${date.getMonth() + 1}\/${date.getFullYear()} ${hour}:${date.getMinutes()}:${date.getSeconds()}`})
-  fs.writeFile('../../../data/tendanceTwitch.json', JSON.stringify({twitchTrendArray}, null, 2), (err) => {
+  fs.writeFile('src/data/tendanceTwitch.json', JSON.stringify({twitchTrendArray}, null, 2), (err) => {
     if (err) throw err;
     else console.log('File is write successfully.');
   });
